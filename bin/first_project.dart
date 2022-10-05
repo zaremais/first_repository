dfab97dbc0d4c0ae289c8ba9e23c9370277cbb92
Lia <admin@lia-meow.local>
2022-10-05 20:36:29 +0600

    add first project

---
+++ bin/first_project.dart
@@ -0,0 +1,6 @@
+import 'package:first_project/first_project.dart' as first_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_project.calculate()}!');
+  print('Zarema');
+}
